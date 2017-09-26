4783098af53913661f1fc3402cf23d33d3e2eade
abeerels <abber2000g@gmail.com>
2017-09-26 15:42:35 +0300

    push all files

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.mous-box').hover(function () {
+
+        $(this).find('.qick-view').show();
+
+
+
+    },function () {
+        $(this).find('.qick-view').hide();
+
+
+    });
+
+});
